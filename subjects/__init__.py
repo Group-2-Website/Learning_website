1bed228b4576a853990f9dfb9550160a467130f7
Diala Assaf Kourieh <diala.assafkourieh@students.fhnw.ch>
2026-04-01 13:40:08 +0200

    Add Science Subject with two topics Geography and Biology without any data . also add csv files for the database.

--- subjects/__init__.py
+++ subjects/__init__.py
@@ -1,11 +1,12 @@
 from subjects.language.Language_selection import Language
 from subjects.math.mathematics import Math
+from subjects.science.science import Science
 from models.subject import Subject
 
 # ── Register every subject here.
 # ── main.py imports this list and auto-generates all routes.
 SUBJECTS: list[Subject] = [
-    Math(), Language()
+    Math(), Language(), Science()
 
 ]
 
