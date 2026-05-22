a390e73cf91bbcd9347d398b556bddc7de47ab40
Noor Vinnai <Noorfhnw@users.noreply.github.com>
2026-05-22 23:41:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from subjects import SUBJECTS
 from ui.pages import register_subject
 from subjects.language import tts
 
-tts.init()
 
-for _subject in SUBJECTS:
-    register_subject(_subject)
+def main():
+    tts.init()
 
-app.add_static_files('/images', 'images')
-app.add_static_files('/static', 'static')
+    for _subject in SUBJECTS:
+        register_subject(_subject)
+
+    app.add_static_files('/images', 'images')
+    app.add_static_files('/static', 'static')
+
+    ui.run(title="E-learning for kids", port=8082, reload=False, favicon='static/favicon.png')
 
 
-ui.run(title="E-learning for kids", port=8082, reload=True, favicon='static/favicon.png')
+if __name__ in {"__main__", "__mp_main__"}:
+    main()
